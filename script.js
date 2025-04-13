3fe96cbb3d8cc20c05e1eb76e440a08b0dd1617c
BldrGco <jparkinsonstuart@gmail.com>
2025-04-13 14:45:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-// Newsletter form submission
-document.getElementById('newsletter-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const email = this.querySelector('input').value;
-    alert(`Thank you for subscribing with ${email}!`);
-    this.reset();
-});
